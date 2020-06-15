48684e83b759d25a9dfaa5ecdb8f28577e661777
Bodal-Andrey <a-v-bodal@yandex.ru>
2020-06-15 10:58:17 +0500

    Вносит правки, рекомендованные наставником

--- src/components/card/card.test.js
+++ src/components/card/card.test.js
@@ -3,44 +3,12 @@ import renderer from "react-test-renderer";
 import Card from "./card.jsx";
 import {Offers} from "../../mock.js";
 
-describe(`Test Card component`, () => {
-  it(`Test Card with first offer name`, () => {
-    const tree = renderer.create(
-        <Card
-          name={Offers.OFFER_NAMES[0]}
-          onCardNameClick={() => {}}
-        />).toJSON();
+it(`Test Card with first offer name`, () => {
+  const tree = renderer.create(
+      <Card
+        name={Offers.OFFER_NAMES[0]}
+        onCardNameClick={() => {}}
+      />).toJSON();
 
-    expect(tree).toMatchSnapshot();
-  });
-
-  it(`Test Card with second offer name`, () => {
-    const tree = renderer.create(
-        <Card
-          name={Offers.OFFER_NAMES[1]}
-          onCardNameClick={() => {}}
-        />).toJSON();
-
-    expect(tree).toMatchSnapshot();
-  });
-
-  it(`Test Card with third offer name`, () => {
-    const tree = renderer.create(
-        <Card
-          name={Offers.OFFER_NAMES[2]}
-          onCardNameClick={() => {}}
-        />).toJSON();
-
-    expect(tree).toMatchSnapshot();
-  });
-
-  it(`Test Card with fourth offer name`, () => {
-    const tree = renderer.create(
-        <Card
-          name={Offers.OFFER_NAMES[3]}
-          onCardNameClick={() => {}}
-        />).toJSON();
-
-    expect(tree).toMatchSnapshot();
-  });
+  expect(tree).toMatchSnapshot();
 });
