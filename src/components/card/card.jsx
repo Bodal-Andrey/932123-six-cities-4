f499a7b66af18c18f0246bf8d9cd0558d2ee7e4c
Bodal-Andrey <a-v-bodal@yandex.ru>
2020-06-19 15:57:51 +0500

    Добавляет моки в компонент Card

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -2,25 +2,27 @@ import React from "react";
 import PropTypes from "prop-types";
 
 const Card = (props) => {
-  const {name, onCardNameClick} = props;
+  const {offer, onCardNameClick} = props;
+  const {title, photo, price, type, rating, isPremium, isBookmark} = offer;
+  const withOfRating = rating * 20;
 
   return (
     <article className="cities__place-card place-card">
-      <div className="place-card__mark">
+      {isPremium && <div className="place-card__mark">
         <span>Premium</span>
-      </div>
+      </div>}
       <div className="cities__image-wrapper place-card__image-wrapper">
         <a href="#">
-          <img className="place-card__image" src="img/apartment-01.jpg" width={260} height={200} alt="Place image" />
+          <img className="place-card__image" src={photo} width={260} height={200} alt="Place image" />
         </a>
       </div>
       <div className="place-card__info">
         <div className="place-card__price-wrapper">
           <div className="place-card__price">
-            <b className="place-card__price-value">€120</b>
+            <b className="place-card__price-value">€{price}</b>
             <span className="place-card__price-text">/&nbsp;night</span>
           </div>
-          <button className="place-card__bookmark-button button" type="button">
+          <button className={`place-card__bookmark-button button ${isBookmark ? ` place-card__bookmark-button--active` : ``}`} type="button">
             <svg className="place-card__bookmark-icon" width={18} height={19}>
               <use xlinkHref="#icon-bookmark" />
             </svg>
@@ -29,21 +31,29 @@ const Card = (props) => {
         </div>
         <div className="place-card__rating rating">
           <div className="place-card__stars rating__stars">
-            <span style={{width: `80%`}} />
+            <span style={`width: ${withOfRating}%`} />
             <span className="visually-hidden">Rating</span>
           </div>
         </div>
         <h2 className="place-card__name" onClick={onCardNameClick}>
-          <a href="#">{name}</a>
+          <a href="#">{title}</a>
         </h2>
-        <p className="place-card__type">Apartment</p>
+        <p className="place-card__type">{type}</p>
       </div>
     </article>
   );
 };
 
 Card.propTypes = {
-  name: PropTypes.string.isRequired,
+  offer: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    photo: PropTypes.string.isRequired,
+    price: PropTypes.number.isRequired,
+    type: PropTypes.string.isRequired,
+    rating: PropTypes.number.isRequired,
+    isPremium: PropTypes.bool.isRequired,
+    isBookmark: PropTypes.bool.isRequired,
+  }),
   onCardNameClick: PropTypes.func.isRequired,
 };
 
