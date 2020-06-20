ff900974650adee40eeb933129eefe49414b64a1
Bodal-Andrey <a-v-bodal@yandex.ru>
2020-06-20 19:06:09 +0500

    Изменяет компонент App

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -4,7 +4,6 @@ import PropTypes from "prop-types";
 const Card = (props) => {
   const {offer, onCardClick} = props;
   const {title, photo, price, type, rating, isPremium, isBookmark} = offer;
-  const withOfRating = rating * 20;
 
   return (
     <article className="cities__place-card place-card">
@@ -26,12 +25,12 @@ const Card = (props) => {
             <svg className="place-card__bookmark-icon" width={18} height={19}>
               <use xlinkHref="#icon-bookmark" />
             </svg>
-            <span className="visually-hidden">To bookmarks</span>
+            <span className="visually-hidden">{isBookmark ? `In bookmarks` : `To bookmarks`}</span>
           </button>
         </div>
         <div className="place-card__rating rating">
           <div className="place-card__stars rating__stars">
-            <span style={{width: `${withOfRating}%`}} />
+            <span style={{width: Math.round(rating) * 20 + `%`}} />
             <span className="visually-hidden">Rating</span>
           </div>
         </div>
