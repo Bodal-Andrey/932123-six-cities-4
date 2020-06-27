d13e4032e2bb678192a26a4af3e17f467536c965
Bodal-Andrey <a-v-bodal@yandex.ru>
2020-06-27 13:36:24 +0500

    Дополняет моки и тестовые дынные

--- src/components/reviews-list/reviews-list.jsx
+++ src/components/reviews-list/reviews-list.jsx
@@ -2,14 +2,16 @@ import React from "react";
 import PropTypes from "prop-types";
 import ReviewsItem from "../reviews-item/reviews-item.jsx";
 
-const ReviewsList = (props) => {
-  const {offer} = props;
+const ReviewsList = ({offer}) => {
+  const {additional} = offer;
 
   return (
     <section className="property__reviews reviews">
       <h2 className="reviews__title">Reviews · <span className="reviews__amount">1</span></h2>
       <ul className="reviews__list">
-        <ReviewsItem offer={offer} />
+        {additional.map((it, i) => (
+          <ReviewsItem key={`${i}-${it}`} offer={it} />
+        ))}
       </ul>
       <form className="reviews__form form" action="#" method="post">
         <label className="reviews__label form__label" htmlFor="review">Your review</label>
@@ -58,7 +60,11 @@ const ReviewsList = (props) => {
 };
 
 ReviewsList.propTypes = {
-  offer: PropTypes.object.isRequired,
+  offer: PropTypes.shape(
+      PropTypes.shape({
+        additional: PropTypes.array.isRequired,
+      })
+  ).isRequired,
 };
 
 export default ReviewsList;
